71715ee6ce418d34be1e09384b9ee34c3f93e5a2
mariopce <mariopce@gmail.com>
2012-08-31 01:16:06 +0200

    adding test project

---
+++ SpeakerMeterTest/src/pl/mobilization/speakermeter/SpeakerTest.java
@@ -0,0 +1,13 @@
+package pl.mobilization.speakermeter;
+
+import junit.framework.Assert;
+import android.test.AndroidTestCase;
+
+public class SpeakerTest extends AndroidTestCase {
+
+	
+	
+	public void testAaaa(){
+		Assert.assertEquals(true,true);
+	}
+}
