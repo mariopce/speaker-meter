94a71c428cb86660a54942642a18449735db835b
Marek Defecinski <marekdef@gmail.com>
2012-09-11 06:53:21 +0200

    Deleted unused

--- src/pl/mobilization/speakermeter/ResultListener.java
+++
@@ -1,7 +0,0 @@
-package pl.mobilization.speakermeter;
-
-import pl.mobilization.speakermeter.downloader.AbstractDownloader;
-
-public interface ResultListener<T> {
-	public void resultAvailable(AbstractDownloader<T> downloader);
-}
